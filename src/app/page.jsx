418ee97da502fdab5b95a3fd8749bd4c718c13ac
Rupam Das <150364981+ItzFalco07@users.noreply.github.com>
2025-03-04 04:56:05 +0000

    made responsive (landing page)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,14 @@ import Pages from '@/components/Pages'
 import Footer from '@/components/Footer'
 
 export default function Home() {
+  
   return (
-    <div className='con relative'>
+    <div className="px-[1em] relative sm:px-[2em] md:px-[4em]  2xl:max-w-[70vw] 2xl:mx-auto">
       <Navbar />
       <Hero />
       <Pages />
       <Usage />
-      <Footer />
+      <Footer /> 
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Hero = () => {
+
     return (
     <>
         <div className='min-h-screen flex items-center'>
             <div className='pt-10'>
-                <h1 className='text-6xl font-bold w-[600px] dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b from-white to-zinc-300'>Beautiful & Modern ui components for your website</h1>
-                <p className='mt-4 text-xl w-[400px]'>Say bye to unnecessory Tailwind class mess, just copy and paste</p>
-                <Link href="/docs/overview"><Button className='mt-8 h-11 font-[600]'>Components <ChevronRight /></Button></Link>
-                <Image alt='logos' className='mt-12' src='/logos.svg' width={500} height={500} />
+                <h1 className='text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b from-white to-zinc-300'>Beautiful & Modern <br/> ui components for <br/> your website</h1>
+                <p className='mt-4 text-sm max-w-xs md:text-md md:max-w-sm lg:text-xl'>Say bye to unnecessory Tailwind class mess, just copy and paste</p>
+              
+                <Link href="/docs/overview"><Button className='mt-8 px-2 py-1 text-xs lg:px-4 lg:py-[1.6em] lg:text-[0.85em] font-[600]'>Components <ChevronRight /> </Button></Link>
+                <Image alt='logos' className='mt-12 max-w-xs sm:max-w-sm lg:max-w-lg' src='/logos.svg' width={500} height={500} />
             </div>
         </div>
         
