0f8934ebec9b76a42d8f08f90453b43b528c577a
ItzFalco08 <somadas91258@gmail.com>
2025-05-24 11:01:40 +0000

    Pending changes exported from your codespace

---
+++ src/components/Selector.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Selector = () => {
+  return (
+    <div className='w-full grid grid-cols-2 gap-2 my-4'>
+        <div className='h-[100px] bg-background'>
+        </div>
+        <div className='h-[100px] bg-secondary'>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Selector
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,6 +17,9 @@ const Sidebar = () => {
                 {
                     title: "Installation",
                 },
+                {
+                    title: "Dark Mode",
+                },
             ],
         },
 
